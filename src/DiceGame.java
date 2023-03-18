e39025d11cdd7b130b2ee227f85ac047b69aa19e
Anas Bashir <55910194+an4s911@users.noreply.github.com>
2023-03-18 20:35:18 +0300

    Completed documenting the whole code

--- src/DiceGame.java
+++ src/DiceGame.java
@@ -35,10 +35,11 @@ public class DiceGame {
 		char choice = getChoiceFromPlayer("Ready to begin? (Type 'y' when you're ready)");
 
 		if (choice != 'y') {
+			// Player wasn't ready to begin
 			return;
 		}
 
-		while (true) {
+		while (true) { // Main game loop, until either player or computer wins
 			boolean playerWins = emulatePlayerTurn();
 			if (playerWins) {
 				printScores();
@@ -64,7 +65,7 @@ public class DiceGame {
 	 * @return boolean true if the player has won, false otherwise
 	 */
 	public static boolean emulatePlayerTurn() {
-		boolean running = true;
+		boolean running = true; // varialbe that indicates the status of the player's turn
 		while (running) {
 			System.out.println("You're rolling the dice...");
 			playerDice.roll();
@@ -72,34 +73,38 @@ public class DiceGame {
 
 			int validation = validateRoll(playerDice);
 			if (validation > 0) {
+				// Did not get any 1's, so the player continues if the player hasn't won yet
 				playerTurnTotal += playerDice.getDiceSum();
 
 				if (checkPlayerWin()) {
 					playerGrandTotal += playerTurnTotal;
-					return true;
+					return true; // The player wins
 				}
 
-				printPlayerScores();
+				printPlayerScores(); // Prints the player's scores if it hasn't won yet
 				char choice = getChoiceFromPlayer("Do you want to continue rolling? (Type 'y' or 'n')");
-				running = (choice == 'y');
+				running = (choice == 'y'); // turn over dice if the player doesn't want to continue rolling
 
 			} else if (validation == 0) {
+				// Got one 1 in dice roll, player loses the turn score and turns over the dice
 				System.out.println("You got a 1!");
 				playerTurnTotal = 0;
 			} else {
+				// Got two 1's, player loses the total score and turns over the dice
 				System.out.println("You got two 1's!");
 				playerTurnTotal = 0;
 				playerGrandTotal = 0;
 			}
 
 			if (validation <= 0) {
+				// If the player got at least one 1, the player turns over the dice
 				getChoiceFromPlayer("Continue? (Type 'y' when you're ready to turn the dice over to me)");
 				running = false;
 			}
 		}
 
-		playerGrandTotal += playerTurnTotal;
-		playerTurnTotal = 0;
+		playerGrandTotal += playerTurnTotal; // Increment the player's grand total
+		playerTurnTotal = 0; // reset the player's turn total
 
 		printScores();
 
@@ -114,7 +119,7 @@ public class DiceGame {
 	 * @return boolean true if the computer has won, false otherwise
 	 */
 	public static boolean emulateComputerTurn() {
-		boolean running = true;
+		boolean running = true; // variable that indicates the status of the computer's turn
 		while (running) {
 			System.out.println("I'm rolling the dice...");
 			computerDice.roll();
@@ -122,38 +127,45 @@ public class DiceGame {
 
 			int validation = validateRoll(computerDice);
 			if (validation > 0) {
+				// Did not get any 1's, so the computer continues if it hasn't won yet
 				computerTurnTotal += computerDice.getDiceSum();
+
 				if (checkComputerWin()) {
 					computerGrandTotal += computerTurnTotal;
-					return true;
+					return true; // The computer has won
 				}
 
-				printComputerScores();
+				printComputerScores(); // print the computer's scores if it didn't win yet
 			} else if (validation == 0) {
+				// Got one 1 in dice roll, computer loses its turn score and turns over the dice
 				System.out.println("I got a 1!");
 				computerTurnTotal = 0;
 			} else {
+				// Got two 1's, computer loses its total score and turns over the dice
 				System.out.println("I got two 1's");
 				computerTurnTotal = 0;
 				computerGrandTotal = 0;
 			}
 
 			if (computerTurnTotal >= 20) {
+				// Computer accumulated more than 20 points in the current turn
 				System.out.println("I got more than 20 in this turn!");
 			}
 
 			if (validation <= 0 || computerTurnTotal >= 20) {
+				// If the computer got at least one 1, or if the computer got more than 20
+				// points in the current turn, then turn over the dice
 				getChoiceFromPlayer("Continue? (Type 'y' when you're ready)");
 				running = false;
 			}
 		}
 
-		computerGrandTotal += computerTurnTotal;
-		computerTurnTotal = 0;
+		computerGrandTotal += computerTurnTotal; // Increment the computer's total score
+		computerTurnTotal = 0; // reset the computer's turn score
 
 		printScores();
 
-		return false;
+		return false; // the computer hasn't won yet
 	}
 
 	/**
